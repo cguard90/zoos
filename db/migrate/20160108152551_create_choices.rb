4ab9e82cf26a5d96c327b15fdbce2f278c50151a
Carolina Medellin & Chris Guard & Peter Wood <pair+caromedellin.cguard90.roytuesday@devbootcamp.com>
2016-01-08 16:39:31 -0500

    Add missing commas to migrations

--- db/migrate/20160108152551_create_choices.rb
+++ db/migrate/20160108152551_create_choices.rb
@@ -1,7 +1,7 @@
 class CreateChoices < ActiveRecord::Migration
   def change
     create_table :choices do |t|
-      t.string :value null: false
+      t.string :value, null: false
       t.timestamps null: false
   end
 end
