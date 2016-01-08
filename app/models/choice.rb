6d988ae46a316355259718f3c72725d6d8580083
Carolina Medellin & Chris Guard & Peter Wood <pair+caromedellin.cguard90.roytuesday@devbootcamp.com>
2016-01-08 16:11:20 -0500

    add relations to models

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,3 +1,7 @@
 class Choice < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :potential_replies
+  has_many :questions through: :potential_replies
+
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   # Remember to create a migration!
+    has_many :potential_replies
+    has_many :choices through: :potential_replies
+    belongs_to :survey
 end
