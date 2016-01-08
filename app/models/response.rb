07fe71721de6794950c2875d84f64dbf14c9045a
Carolina Medellin & Chris Guard & Peter Wood <pair+caromedellin.cguard90.roytuesday@devbootcamp.com>
2016-01-08 17:35:02 -0500

    Update models with 'foreign_key' field on all needed relations

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,6 +3,7 @@ class Response < ActiveRecord::Base
   belongs_to :user, foreign_key: :responder_id
   belongs_to :survey
   belongs_to :question
+  has_one :choice, through: :potential_reply
 
   validates :potential_reply_id, presence: true, numericality: {only_integer: true}
   validates :responder_id, presence: true, numericality: {only_integer: true}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :surveys
+  has_many :surveys, foreign_key: :creator_id
+  has_many :responses, foreign_key: :responder_id
   has_many :questions, through: :surveys
-  has_many :responses
 
   validates :email, presence: true, uniqueness: true
   validates :password_hash, presence: true
