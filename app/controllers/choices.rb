4cf654f815d67fde4bd3476506737ab2ada8ba97
Peter Wood <email": null>
2016-01-09 16:11:08 -0500

    Update choice routes to include current survey for new choices

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -1,10 +1,12 @@
 get '/choices/new' do
   @choice_numbers = (1..params[:question_choices].to_i).to_a
   @question = Question.create(params[:question])
+  @survey = @question.survey
   erb :'/choices/new'
 end
 
 post '/choices' do
+  @survey = Survey.find_by(id: params[:survey_id])
   @choice = Choice.create(params[:choice])
   erb :'/choices/new'
 end
