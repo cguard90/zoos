9154df116922449583d5f8b8d1f55418acd3eb65
caromedellin <caromedellin@gmail.com>
2016-01-09 13:09:48 -0500

    add route to questions new and basic survey new page complete

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -3,6 +3,20 @@ get '/surveys' do
   erb :"surveys/index"
 end
 
+get '/surveys/new' do
+  erb :"surveys/new"
+end
+
+post '/surveys' do
+  @survey = Survey.new(params[:survey])
+  if @survey.save
+    redirect'/questions/new'
+  else
+    @error = @survey.errors.full_messages
+    erb :'/survey/new'
+  end
+end
+
 get '/surveys/:id' do
   @survey = Survey.find(params[:id])
   erb :"surveys/show"
