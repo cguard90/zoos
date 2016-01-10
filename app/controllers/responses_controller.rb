376b52434204a867c63974bc072b5a3c981f2af3
Chris Guard <cguard90@gmail.com>
2016-01-10 10:38:27 -0500

    Survey moves from one question to the next, and creates each answer based on reply

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,12 +1,22 @@
 
 post '/answers' do
   question = Question.find_by(id: params[:answer][:question_id])
+
   potential_reply = PotentialReply.find_by(question_id: question.id,
-                                          choice_id: params[:answer][:choice_id])
+                            choice_id: params[:answer][:choice_id])
+
   @answer = Answer.new(user: current_user,
                            question: question,
                            survey: question.survey,
                            potential_reply: potential_reply)
+
   @errors = @answer.errors.full_messages unless @answer.save
-    redirect '/questions'
+
+  next_question =  Question.find_by(id: (@answer.question.id+1))
+  if next_question.survey == @answer.survey
+    redirect "/questions/#{next_question.id}"
+  else
+    binding.pry
+    redirect '/'
+  end
 end
